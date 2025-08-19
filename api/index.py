fdd9d1d2e44507f62b0407c6207e03426dab59fb
Krishna <rkriscan2017@gmail.com>
2025-08-19 13:46:44 +0530

    Final comprehensive fix for index.py from corrected_index.txt

--- api/index.py
+++ api/index.py
@@ -46,7 +46,7 @@ def get_instrument_list():
         with open(INSTRUMENT_LIST_PATH, 'r') as f:
             return json.load(f)
         
-    url = "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json"
+    url = "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaste.json"
     try:
         response = requests.get(url)
         response.raise_for_status()
